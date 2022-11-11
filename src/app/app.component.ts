b9f888b50a4223bde731257fe3e39778a9d66da7
anirbmuk <anirbmuk@live.in>
2022-11-11 15:52:33 +0100

    feat: added auto reload on service worker update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID
+} from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'ttd-root',
@@ -6,4 +15,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private readonly update: SwUpdate,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    @Inject(PLATFORM_ID) readonly platformId: Object,
+    @Inject(DOCUMENT) readonly document: Document
+  ) {}
+
+  ngOnInit(): void {
+    this.update.versionUpdates.pipe(take(1)).subscribe(async () => {
+      try {
+        await this.update.activateUpdate();
+        this.document.location.reload();
+      } catch (err) {
+        console.error('[AppComponent] [ngOnInit] [versionUpdates]', err);
+      }
+    });
+  }
+}
