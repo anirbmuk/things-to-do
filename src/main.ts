c78f3e11737b0843ca5bd43cce2d6991fa975d14
anirbmuk <anirbmuk@live.in>
2023-05-20 00:01:26 +0200

    chore: named imports for relative paths

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app/app-routing.module';
 
+import { environment } from '@environment';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
 
 import { MatDialogModule } from '@angular/material/dialog';
 
